8f5e6a42ec355c559c39e9648e056a3f9b5ea74a
orulayse <76255789+orulayse@users.noreply.github.com>
2020-12-21 14:02:48 +0300

    Create anaSayfa.dart

---
+++ anaSayfa.dart
@@ -0,0 +1,402 @@
+import 'package:flutter/material.dart';
+
+class anaSayfa extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+              SizedBox(height: 10, ),
+              Container(child: Text("Ayşegül Örül", style: TextStyle(color: Colors.white, fontSize: 20 ), ),
+              ),
+              SizedBox(height: 10, ),
+            Container(              
+              child: Row(
+                children: [
+                  Container(
+                 alignment: Alignment.bottomRight,
+                   width:90.0,
+                   height:100.0,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      border: Border.all(
+                        color: Colors.black,
+                      ),
+                    ),
+                    child: Text("1",style: TextStyle(color: Colors.black,),
+                    ),
+                  ),
+                  Container(
+                       alignment: Alignment.bottomRight,
+                    width: 90.0,
+                    height: 100.0,
+                    decoration: BoxDecoration(
+                      color: Colors.brown,
+                      border: Border.all(
+                        color: Colors.black,
+                      ),
+                    ),
+                    child: Text(
+                      "2",
+                      style: TextStyle(
+                        color: Colors.black,
+                      ),
+                    ),
+                  ),
+                  Container(
+                       alignment: Alignment.bottomRight,
+                    width: 90.0,
+                    height: 100.0,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      border: Border.all(
+                        color: Colors.black,
+                      ),
+                    ),
+                    child: Text(
+                      "3",
+                      style: TextStyle(
+                        color: Colors.black,
+                      ),
+                    ),
+                  ),
+                  Container(
+                       alignment: Alignment.bottomRight,
+                    width: 90.0,
+                    height: 100.0,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      border: Border.all(
+                        color: Colors.black,
+                      ),
+                    ),
+                    child: Text(
+                      "4",
+                      style: TextStyle(
+                        color: Colors.black,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+                
+              
+              child: Row(
+                children: [
+                  Container(
+                       alignment: Alignment.bottomRight,
+                    width: 90.0,
+                    height: 100.0,
+                    decoration: BoxDecoration(
+                      color: Colors.brown,
+                      border: Border.all(
+                        color: Colors.black,
+                      ),
+                    ),
+                    child: Text(
+                      "5",
+                      style: TextStyle(
+                        color: Colors.black,
+                      ),
+                    ),
+                  ),
+                  Container(
+                       alignment: Alignment.bottomRight,
+                    width: 90.0,
+                    height: 100.0,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      border: Border.all(
+                        color: Colors.black,
+                      ),
+                    ),
+                    child: Text(
+                      "6",
+                      style: TextStyle(
+                        color: Colors.black,
+                      ),
+                    ),
+                  ),
+                  Container(
+                       alignment: Alignment.bottomRight,
+                    width: 90.0,
+                    height: 100.0,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      border: Border.all(
+                        color: Colors.black,
+                      ),
+                    ),
+                    child: Text(
+                      "7",
+                      style: TextStyle(
+                        color: Colors.black,
+                      ),
+                    ),
+                  ),
+                  Container(
+                       alignment: Alignment.bottomRight,
+                    width: 90.0,
+                    height: 100.0,
+                    decoration: BoxDecoration(
+                      color: Colors.brown,
+                      border: Border.all(
+                        color: Colors.black,
+                      ),
+                    ),
+                    child: Text(
+                      "8",
+                      style: TextStyle(
+                        color: Colors.black,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+               
+              child: Row(
+                children: [
+                  Container(
+                      alignment: Alignment.bottomRight,
+                    width: 90.0,
+                    height: 100.0,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      border: Border.all(
+                        color: Colors.black,
+                      ),
+                    ),
+                    child: Text(
+                      "9",
+                      style: TextStyle(
+                        color: Colors.black,
+                      ),
+                    ),
+                  ),
+                  Container(
+                       alignment: Alignment.bottomRight,
+                    width: 90.0,
+                    height: 100.0,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      border: Border.all(
+                        color: Colors.black,
+                      ),
+                    ),
+                    child: Text(
+                      "10",
+                      style: TextStyle(
+                        color: Colors.black,
+                      ),
+                    ),
+                  ),
+                  Container(
+                       alignment: Alignment.bottomRight,
+                    width: 90.0,
+                    height: 100.0,
+                    decoration: BoxDecoration(
+                      color: Colors.brown,
+                      border: Border.all(
+                        color: Colors.black,
+                      ),
+                    ),
+                    child: Text(
+                      "11",
+                      style: TextStyle(
+                        color: Colors.black,
+                      ),
+                    ),
+                  ),
+                  Container(
+                       alignment: Alignment.bottomRight,
+                    width: 90.0,
+                    height: 100.0,
+                    decoration: BoxDecoration(
+                      color: Colors.brown,
+                      border: Border.all(
+                        color: Colors.black,
+                      ),
+                    ),
+                    child: Text(
+                      "12",
+                      style: TextStyle(
+                        color: Colors.black,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              
+              child: Row(
+                children: [
+                  Container(
+                       alignment: Alignment.bottomRight,
+                    width: 90.0,
+                    height: 100.0,
+                    decoration: BoxDecoration(
+                      color: Colors.brown,
+                      border: Border.all(
+                        color: Colors.black,
+                      ),
+                    ),
+                    child: Text(
+                      "13",
+                      style: TextStyle(
+                        color: Colors.black,
+                      ),
+                    ),
+                  ),
+                  Container(
+                       alignment: Alignment.bottomRight,
+                    width: 90.0,
+                    height: 100.0,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      border: Border.all(
+                        color: Colors.black,
+                      ),
+                    ),
+                    child: Text(
+                      "14",
+                      style: TextStyle(
+                        color: Colors.black,
+                      ),
+                    ),
+                  ),
+                  Container(
+                       alignment: Alignment.bottomRight,
+                    width: 90.0,
+                    height: 100.0,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      border: Border.all(
+                        color: Colors.black,
+                      ),
+                    ),
+                    child: Text(
+                      "15",
+                      style: TextStyle(
+                        color: Colors.black,
+                      ),
+                    ),
+                  ),
+                  Container(
+                       alignment: Alignment.bottomRight,
+                    width: 90.0,
+                    height: 100.0,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      border: Border.all(
+                        color: Colors.black,
+                      ),
+                    ),
+                    child: Text(
+                      "16",
+                      style: TextStyle(
+                        color: Colors.black,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+               
+              child: Row(
+                children: [
+                  Container(
+                      alignment: Alignment.bottomRight,
+                    width: 90.0,
+                    height: 100.0,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      border: Border.all(
+                        color: Colors.black,
+                      ),
+                    ),
+                    child: Text(
+                      "17",
+                      style: TextStyle(
+                        color: Colors.black,
+                      ),
+                    ),
+                  ),
+                  Container(
+                       alignment: Alignment.bottomRight,
+                    width: 90.0,
+                    height: 100.0,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      border: Border.all(
+                        color: Colors.black,
+                      ),
+                    ),
+                    child: Text(
+                      "18",
+                      style: TextStyle(
+                        color: Colors.black,
+                      ),
+                    ),
+                  ),
+                  Container(
+                       alignment: Alignment.bottomRight,
+                    width: 90.0,
+                    height: 100.0,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      border: Border.all(
+                        color: Colors.black,
+                      ),
+                    ),
+                    child: Text(
+                      "19",
+                      style: TextStyle(
+                        color: Colors.black,
+                      ),
+                    ),
+                  ),
+                  Container(
+                       alignment: Alignment.bottomRight,
+                    width: 90.0,
+                    height: 100.0,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      border: Border.all(
+                        color: Colors.black,
+                      ),
+                    ),
+                    child: Text(
+                      "20",
+                      style: TextStyle(
+                        color: Colors.black,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 10.0 ),
+            Container(child:Text("202855028",style: TextStyle(
+                        color: Colors.white,
+                      ),),),
+                      SizedBox(height: 10.0 ),
+            Container(child:Text("32%20 = 12",style: TextStyle(
+                        color: Colors.white,
+                      ),),),
+                      SizedBox(height: 10.0 ),
+            Container(child:Text("11-12-13",style: TextStyle(
+                        color: Colors.white,
+                      ),),),
+          ],
+        ),
+      ), 
+    );     
+  }
+}
